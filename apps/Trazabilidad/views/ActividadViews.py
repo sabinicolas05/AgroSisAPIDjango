6f505112834f87d2267193a4c833709422e672d6
vargas918 <jhoanvargas4848@gmail.com>
2024-12-01 16:15:39 -0500

    Correccion de ActividadVieSet

--- apps/Trazabilidad/views/ActividadViews.py
+++ apps/Trazabilidad/views/ActividadViews.py
@@ -1,7 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from ..models.Actividad import Actividad
 from ..serializers.ActividadSerializer import ActividadSerializer
 
 class ActividadViews(ModelViewSet):
     queryset = Actividad.objects.all()
-    fields = '__all__'
+    serializer_class = ActividadSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
