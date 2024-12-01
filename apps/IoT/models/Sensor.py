7f1858b2fa11b9b564239d19f8b9c30f02641329
sabinicolas05 <nsabirojas@gmail.com>
2024-11-30 22:19:19 -0500

    Tabla configuración terminada, corrección en tabla Sensor (cambio en la función __str__)

--- apps/IoT/models/Sensor.py
+++ apps/IoT/models/Sensor.py
@@ -7,4 +7,4 @@ class Sensor(models.Model):
     fk_tipo_sensor = models.ForeignKey(Tipo_Sensor,on_delete=models.SET_NULL,null=True)
     fk_configuracion = models.ForeignKey(Configuracion,on_delete=models.SET_NULL,null=True)
     def __str__(self):
-        return str(self.fk_tipo_sensor+self.fk_bancal)
+        return str(self.fk_tipo_sensor) + str(self.fk_bancal)
