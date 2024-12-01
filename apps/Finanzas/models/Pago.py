a0c7b8af34c41c765526b2d33d1dea7643123ead
sabinicolas05 <nsabirojas@gmail.com>
2024-12-01 17:12:43 -0500

    Correción de importación al serializador de Asignacion_Actividad

--- apps/Finanzas/models/Pago.py
+++ apps/Finanzas/models/Pago.py
@@ -1,5 +1,5 @@
 from django.db import models
-from apps.Trazabilidad.models.Asignacion_actividad import Asignacion_Actividad
+from apps.Trazabilidad.models.Asignacion_Actividad import Asignacion_Actividad
 
 class Pago(models.Model):
     fk_asignacion_actividad = models.ForeignKey(Asignacion_Actividad,on_delete=models.SET_NULL,null=True)
