a3b1126a85ac548c6cb0c0b0e98000cb579715bb
juancc9 <sicsound00@gmail.com>
2024-12-19 12:37:28 -0500

    "eliminacion de redundancias, creacion de ruta a usuarios"

--- apps/Users/routers/UsuarioRouter.py
+++ apps/Users/routers/UsuarioRouter.py
@@ -1,5 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from ..views.UsuarioViews import UsuarioViews
 
+
 UsuarioRouter= DefaultRouter()
 UsuarioRouter.register(prefix='usuario', viewset=UsuarioViews, basename="usuario")
